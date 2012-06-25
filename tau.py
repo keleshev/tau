a76e77f625e87c2ed3e5f128046fb17311278da8
Vladimir Keleshev <halst@ya.ru>
2012-06-25 18:40:38 +0200

    DumpCenter fork: Tau database.

---
+++ tau.py
@@ -0,0 +1,196 @@
+#! /usr/bin/env python
+"""tau: time series database
+
+       .##########
+      ############
+     #    :##
+          ##,
+          ##,   ,
+          ######
+          `####+
+
+Usage:
+  tau (-h | --help | --version)
+  tau server start [--port=<port>]
+  tau server stop
+      [--host=<host>] [--port=<port>]
+  tau set <key=value> ...
+      [--host=<host>] [--port=<port>]
+  tau get <key> ... [--timestamps] [--period=<seconds>]
+      [--host=<host>] [--port=<port>]
+  tau clear
+      [--host=<host>] [--port=<port>]
+
+Options:
+  --host=<host>  [default: localhost]
+  --port=<port>  [default: 6283]
+
+"""
+import socket
+import json
+from fnmatch import fnmatchcase
+from datetime import datetime
+
+from docopt import docopt
+
+
+class TauProtocol(object):
+
+    def __init__(self, host='localhost', port=1042, client=None):
+        self._host = host
+        self._port = port
+        self._client = client
+
+    def __enter__(self):
+        if not self._client:
+            self._client = socket.socket()
+            self._client.connect((self._host, self._port))
+        return self
+
+    def send(self, message):
+        def encode_datetime(obj):
+            if isinstance(obj, datetime):
+                return {'__datetime__': str(obj)}
+            raise TypeError(repr(obj) + " is not JSON serializable")
+        #assert '\n' not in json.dumps(message)
+        self._client.send(json.dumps(message, default=encode_datetime) + '\n')
+
+    def receive(self):
+        def decode_datetime(obj):
+            if '__datetime__' in obj:
+                return datetime.strptime(obj['__datetime__'],
+                                         '%Y-%m-%d %H:%M:%S.%f')
+            return obj
+        message = ''
+        while True:
+            message += self._client.recv(4096)
+            if message.endswith('\n'): # or message == '':
+                break
+        return json.loads(message, object_hook=decode_datetime)
+
+    def __exit__(self, exception_type, value, traceback):
+        #self._client.shutdown(socket.SHUT_RDWR)
+        self._client.close()
+
+
+class TauServer(object):
+
+    def __init__(self, port=1042, lifetime=1):
+        try:
+            self.tau = Tau(lifetime)
+            self.server = socket.socket()
+            #self.server.bind((socket.gethostname(), port))
+            self.server.bind(('', port))
+            self.server.listen(5)
+            while True:
+                client, address = self.server.accept()
+                with TauProtocol(client=client) as protocol:
+                    command, argument = protocol.receive()
+                    if command == 'get':
+                        protocol.send(self.tau.get(*argument[0],
+                                                   **argument[1]))
+                    elif command == 'set':
+                        self.tau.set(argument)
+                    elif command == 'clear':
+                        self.tau.clear()
+        finally:
+            #self.server.shutdown(socket.SHUT_RDWR)
+            self.server.close()
+
+
+class TauClient(object):
+
+    def __init__(self, host='localhost', port=6283):
+        self.host = host
+        self.port = port
+
+    def set(self, *arg, **kw):
+        data = arg[0] if arg else kw
+        with TauProtocol(self.host, self.port) as protocol:
+            protocol.send(['set', data])
+
+    def get(self, *arg, **kw):
+        with TauProtocol(self.host, self.port) as protocol:
+            protocol.send(['get', [arg, kw]])
+            return protocol.receive()
+
+    def clear(self):
+        with TauProtocol(self.host, self.port) as protocol:
+            protocol.send(['clear', None])
+
+
+class Tau(object):
+
+    def __init__(self, lifetime=1):
+        self._lifetime = lifetime
+        self._state = {}
+
+    def set(self, *arg, **kw):
+        keyvalues = arg[0] if arg else kw
+        for key, value in keyvalues.items():
+            if key not in self._state:
+                self._state[key] = []
+            self._state[key].append([datetime.now(), value])
+
+    def get(self, *arg, **kw):
+        self._state = self._truncate(self._state, self._lifetime)
+        match = self._get_match(arg)
+        if kw.get('period'):
+            match = self._truncate(match, kw['period'])
+
+        if kw.get('period') and kw.get('timestamps'):
+            transform = lambda x: x if x else []
+        elif kw.get('period'):
+            transform = lambda x: list(zip(*x)[1]) if x else []
+        elif kw.get('timestamps'):
+            transform = lambda x: x[-1] if x else []
+        else:
+            transform = lambda x: x[-1][1] if x else None
+
+        match = dict((key, transform(val)) for key, val in match.items())
+
+        if len(arg) == 1 and not self._is_pattern(arg[0]):
+            return match.get(arg[0])
+        return match
+
+    def _get_match(self, argument):
+        patterns = [a for a in argument if self._is_pattern(a)]
+        keys = [a for a in argument if not self._is_pattern(a)]
+        patterns_match = {}
+        for p in patterns:
+            for k in self._state.keys():
+                if fnmatchcase(k, p):
+                    patterns_match[k] = self._state.get(k)
+        keys_match = dict((key, self._state.get(key)) for key in keys)
+        return dict(patterns_match.items() + keys_match.items())
+
+    @staticmethod
+    def _truncate(state, period):
+        now = datetime.now()
+        for key in state:
+            state[key] = [[t, v] for [t, v] in state[key]
+                          if (now - t).total_seconds() < period]
+        return state
+
+    @staticmethod
+    def _is_pattern(s):
+        return '*' in s or '?' in s or '[' in s or ']' in s
+
+    def clear(self):
+        self._state = {}
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='zero')
+    tau = TauClient(host=args['--host'], port=int(args['--port']))
+    if args['server'] and args['start']:
+        TauServer(port=int(args['--port']))
+    elif args['server'] and args['stop']:
+        tau.stop()
+    elif args['set']:
+        tau.set(dict(kv.split('=') for kv in args['<key=value>']))
+    elif args['get']:
+        print(tau.get(*args['<key>'], period=args['--period'],
+                      timestamps=args['--timestamps']))
+    elif args['clear']:
+        tau.clear()
---
+++ test_tau.py
@@ -0,0 +1,141 @@
+from time import sleep
+from datetime import datetime
+from tau import TauClient
+
+
+def pytest_funcarg__tau(request):
+    tau = TauClient()
+    tau.clear()
+    return tau
+
+
+def test_get_one_value(tau):
+    tau.set(foo=123)
+    assert tau.get('foo') == 123
+
+
+def test_get_compound_value(tau):
+    tau.set(foo={'foo': True, 'bar': None})
+    assert tau.get('foo') == {'foo': True, 'bar': None}
+
+
+def test_set_dict(tau):
+    tau.set({'foo': 123})
+    assert tau.get('foo') == 123
+
+
+def test_get_two_values(tau):
+    tau.set(foo=123, bar=True)
+    assert tau.get('foo', 'bar') == {'foo': 123, 'bar': True}
+
+
+def test_clear_one_value(tau):
+    tau.set(foo=123)
+    assert tau.get('foo') == 123
+    tau.clear()
+    assert tau.get('foo') == None
+
+
+def test_clear_two_values(tau):
+    tau.set(foo=123, bar=True)
+    assert tau.get('foo', 'bar') == {'foo': 123, 'bar': True}
+    tau.clear()
+    assert tau.get('foo', 'bar') == {'foo': None, 'bar': None}
+
+
+def test_get_wilecard(tau):
+    tau.set(foo=123, bar=True)
+    assert tau.get('foo', 'bar') == {'foo': 123, 'bar': True}
+    assert tau.get('*') == {'foo': 123, 'bar': True}
+
+
+def test_get_pattern(tau):
+    tau.set(meanSpeed=123, meanPower=456, foo=False)
+    assert tau.get('mean*') == {'meanSpeed': 123, 'meanPower': 456}
+
+
+def test_get_complex_pattern(tau):
+    tau.set(meanSpeed=1, meanPower=2, rpm1=3, rpm2=4, foo=0)
+    message = {'meanSpeed': 1, 'meanPower': 2, 'rpm1': 3, 'rpm2': 4}
+    assert tau.get('mean*', 'rpm?') == message
+
+
+def test_get_pattern_and_name(tau):
+    tau.set(meanSpeed=1, meanPower=2, rpm1=3, rpm2=4, foo=0)
+    message = {'meanSpeed': 1, 'rpm1': 3, 'rpm2': 4}
+    assert tau.get('rpm[12]', 'meanSpeed') == message
+
+
+def test_gets_latest_value(tau):
+    tau.set(a=0, b=1, foo=-1)
+    tau.set(a=5, b=6, foo=-2)
+    tau.set(a=8, b=9, foo=-3)
+    assert tau.get('?') == {'a': 8, 'b': 9}
+
+
+def test_when_all_values_are_discarded_get_returns_none(tau):
+    tau.set(a=0, b=1, foo=-1)
+    sleep(1)
+    assert tau.get('?') == {'a': None, 'b': None}
+
+
+# Period
+
+
+def test_get_period(tau):
+    tau.set(a=0, b=1, foo=-1)
+    tau.set(a=5, b=6, foo=-2)
+    tau.set(a=8, b=9, foo=-3)
+    assert tau.get('?', period=1) == {'a': [0, 5, 8], 'b': [1, 6, 9]}
+
+
+def test_when_all_values_are_discarded_get_period_returns_empty_lists(tau):
+    tau.set(a=0, b=1, foo=-1)
+    sleep(1)
+    assert tau.get('?', period=9) == {'a': [], 'b': []}
+
+
+def test_old_values_are_discarded_get_period(tau):
+    tau.set(a=0, b=1, foo=-1)
+    sleep(1)
+    tau.set(a=5, b=6, foo=-2)
+    tau.set(a=8, b=9, foo=-3)
+    assert tau.get('?', period=9) == {'a': [5, 8], 'b': [6, 9]}
+
+
+def test_get_period_truncates(tau):
+    tau.set(a=0, b=1, foo=-1)
+    sleep(0.5)
+    tau.set(a=5, b=6, foo=-2)
+    tau.set(a=8, b=9, foo=-3)
+    assert tau.get('?', period=0.5) == {'a': [5, 8], 'b': [6, 9]}
+
+
+# Timestamps
+
+
+def test_get_timestamps(tau):
+    tau.set(a=2, b=3)
+    tau.set(a=8, b=9)
+    d = tau.get('?', timestamps=True)  # {'a': [t, 8], 'b': [t, 9]}
+    assert len(d['a']) == 2
+    assert type(d['a'][0]) == datetime
+    assert d['a'][1] == 8
+    assert len(d['b']) == 2
+    assert type(d['b'][0]) == datetime
+    assert d['b'][1] == 9
+
+
+def test_get_timestamps_with_period(tau):
+    tau.set(a=2, b=3)
+    tau.set(a=8, b=9)
+    # {'a': [[t, 2], [t, 8]], 'b': [[t, 3], [t, 9]]}
+    d = tau.get('?', timestamps=True, period=1)
+    [[t1, v1], [t2, v2]] = d['a']
+    assert t1 < t2
+    assert type(t1) == type(t2) == datetime
+    assert v1 == 2 and v2 == 8
+    [[t1, v1], [t2, v2]] = d['b']
+    assert t1 < t2
+    assert type(t1) == type(t2) == datetime
+    assert v1 == 3 and v2 == 9
