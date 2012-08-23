ce26bb3012b08410b810ab9d8e762240fa20728a
Vladimir Keleshev <halst@ya.ru>
2012-08-23 17:49:29 +0200

    Reorganize tests.

--- test_tau.py
+++ test_tau.py
@@ -1,127 +1,67 @@
 from datetime import datetime
 
-from tau import TauClient
+from tau import Tau, MemoryBackend
 
 
 def pytest_funcarg__tau(request):
-    tau = TauClient()
+    tau = Tau(MemoryBackend())
     tau.clear()
     return tau
 
 
-def test_get_one_value(tau):
+def test_set_get(tau):
     tau.set(foo=123)
     assert tau.get('foo') == 123
-
-
-def test_get_compound_value(tau):
     tau.set(foo={'foo': True, 'bar': None})
     assert tau.get('foo') == {'foo': True, 'bar': None}
-
-
-def test_set_dict(tau):
-    tau.set({'foo': 123})
-    assert tau.get('foo') == 123
-
-
-def test_get_two_values(tau):
+    tau.set({'foo': 3.14})
+    assert tau.get('foo') == 3.14
     tau.set(foo=123, bar=True)
     assert tau.get('foo', 'bar') == {'foo': 123, 'bar': True}
 
 
-def test_clear_one_value(tau):
+def test_clear(tau):
     tau.set(foo=123)
     assert tau.get('foo') == 123
     tau.clear()
     assert tau.get('foo') == None
-
-
-def test_clear_two_values(tau):
     tau.set(foo=123, bar=True)
     assert tau.get('foo', 'bar') == {'foo': 123, 'bar': True}
     tau.clear()
     assert tau.get('foo', 'bar') == {'foo': None, 'bar': None}
 
 
-def test_get_wilecard(tau):
+def test_get_pattern(tau):
     tau.set(foo=123, bar=True)
     assert tau.get('foo', 'bar') == {'foo': 123, 'bar': True}
     assert tau.get('*') == {'foo': 123, 'bar': True}
-
-
-def test_get_pattern(tau):
     tau.set(meanSpeed=123, meanPower=456, foo=False)
     assert tau.get('mean*') == {'meanSpeed': 123, 'meanPower': 456}
-
-
-def test_get_pattern_that_matches_nothing(tau):
     tau.set(foo=123, bar=True)
     assert tau.get('?') == {}
-
-
-def test_get_complex_pattern(tau):
     tau.set(meanSpeed=1, meanPower=2, rpm1=3, rpm2=4, foo=0)
     message = {'meanSpeed': 1, 'meanPower': 2, 'rpm1': 3, 'rpm2': 4}
     assert tau.get('mean*', 'rpm?') == message
-
-
-def test_get_pattern_and_name(tau):
     tau.set(meanSpeed=1, meanPower=2, rpm1=3, rpm2=4, foo=0)
     message = {'meanSpeed': 1, 'rpm1': 3, 'rpm2': 4}
     assert tau.get('rpm[12]', 'meanSpeed') == message
-
-
-def test_gets_latest_value(tau):
-    tau.set(a=0, b=1, foo=-1)
     tau.set(a=5, b=6, foo=-2)
     tau.set(a=8, b=9, foo=-3)
     assert tau.get('?') == {'a': 8, 'b': 9}
 
 
-# Signals
-
-
 def test_signals(tau):
-    tau.clear()
     tau.set(a=0, b=1, foo=-1)
     tau.set(a=5, b=6, foo=-2)
     tau.set(a=8, b=9, foo=-3)
     assert set(tau.signals()) == set(['a', 'b', 'foo'])
 
 
-#def test_matching_signals(tau):
-#    tau.set(a=0, b=1, foo=-1)
-#    tau.set(a=5, b=6, foo=-2)
-#    tau.set(a=8, b=9, foo=-3)
-#    assert tau._matching_signals('?') == set(['a', 'b'])
-#    assert tau._matching_signals('a', '???') == set(['a', 'foo'])
-
-
-#def test_internal_get(tau):
-#    tau.set(a=5, b=6, foo=-2)
-#    tau.set(a=8, b=9, foo=-3)
-#    d = tau._get(['a'], =1)
-#    assert d.keys() == ['a']
-#    [[t1, n], [t2, m]] = d['a']
-#    assert type(t1) == type(t2) == datetime
-#    assert n == 5 and m == 8
-#    assert tau._get(['x'], period=1) == {'x': []}
-
-
-
-
-# Period
-
-
 def test_get_period(tau):
-    tau.clear()
     tau.set(a=0, b=1, foo=-1)
     tau.set(a=5, b=6, foo=-2)
     tau.set(a=8, b=9, foo=-3)
     assert tau.get('?', period=10) == {'a': [0, 5, 8], 'b': [1, 6, 9]}
-
-
-# Timestamps
 
 
 def test_get_timestamps(tau):
@@ -137,7 +77,6 @@ def test_get_timestamps(tau):
 
 
 def test_get_timestamps_with_period(tau):
-    tau.clear()
     tau.set(a=2, b=3)
     tau.set(a=8, b=9)
     # {'a': [[t, 2], [t, 8]], 'b': [[t, 3], [t, 9]]}
@@ -152,12 +91,7 @@ def test_get_timestamps_with_period(tau):
     assert v1 == 3 and v2 == 9
 
 
-# Limit
-
-
 def test_limit(tau):
-    tau.clear()
     for n in range(0, 10):
         tau.set(n=n)
     assert tau.get('n', period=10, limit=7) == [0, 2, 4, 6, 8]
-
